d9eef5d103e9659c5948c1260c5b88de145bcf31
Denwigwe Blossom <bdenwigs@gmail.com>
2026-06-18 20:25:08 +0100

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
